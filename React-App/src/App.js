4a667d68827d97ffeed51494cfae501b0fe93993
Ryan <44690200+ryankamiri@users.noreply.github.com>
2023-11-11 20:39:47 -0800

    Test for DQ

--- React-App/src/App.js
+++ React-App/src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Customize from './components/Customize';
 import Admin from './components/Admin';
+import Test from './components/Test';
 import Checkout from './components/Checkout';
+import CheckoutContext from './context/checkout.context';
+// import Client from 'shopify-buy';
 
 function App() {
+
+  const domain = "project-aphrodite.myshopify.com"
+  const storefrontAccessToken = "a315d6f383a61800bf0af3e9dce49d4a"
+
+  const [checkoutInfo, setCheckoutInfo] = useState({});
+
+  // useEffect(() => {
+  //   let tempWS;
+  //   const initialize = async () => {
+  //       let token = localStorage.getItem("token");
+  //       if (!token){
+  //           localStorage.setItem('token', '');
+  //           token = '';
+  //       }
+  //   }
+  // }, []);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route exact path="/" Component={Customize} />
-        <Route exact path="/admin" Component={Admin} />
-        <Route exact path="/checkout" Component={Checkout} />
-      </Routes>
+      <CheckoutContext.Provider value={{checkoutInfo, setCheckoutInfo}}>
+        <Routes>
+          <Route exact path="/" Component={Customize} />
+          <Route exact path="/admin" Component={Admin} />
+          <Route exact path="/checkout" Component={Checkout} />
+          <Route exact path="/test" Component={Test} />
+        </Routes>
+      </CheckoutContext.Provider>
     </BrowserRouter>
   );
 }
---
+++ React-App/src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+
+  return (
+    <div>
+      <p>Admin</p>
+    </div>
+  );
+};
+
+export default Test;
