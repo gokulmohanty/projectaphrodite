3954aa439bcdd5ac69b9b0fe559bbd823aa8e684
Ryan <44690200+ryankamiri@users.noreply.github.com>
2023-11-12 03:23:12 -0800

    good commit

--- React-App/src/components/Test.js
+++ React-App/src/components/Test.js
@@ -114,6 +114,8 @@ const Test = () => {
 
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
 
+  // const []
+
   return (
     <div className="bg-white">
       <div>
@@ -421,7 +423,24 @@ const Test = () => {
               </form>
 
               {/* Product grid */}
-              <div className="lg:col-span-3">{/* Your content */}</div>
+              <div className="lg:col-span-3">
+                <div key="Name" className="group relative">
+                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                    <img
+                      src={require("../images/fabrics/DSC01291.jpg")}
+                      alt="Alt"
+                      className="h-full w-full object-cover object-center"
+                    />
+                  </div>
+                  <h3 className="mt-6 text-sm text-gray-500">
+                    <a href="">
+                      <span className="absolute inset-0" />
+                      Name
+                    </a>
+                  </h3>
+                  <p className="text-base font-semibold text-gray-900">Description</p>
+                </div>
+              </div>
             </div>
           </section>
         </main>
